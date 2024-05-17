a705de0916185cdd1135abb0a2b186c898f3875d
toshiya-0203 <nishioka.webdesign@gmail.com>
2024-05-17 14:03:56 +0900

    Laravelの基礎を学ぼう-17章

--- laravel/laravel-basic/app/Http/Controllers/ProductController.php
+++ laravel/laravel-basic/app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
@@ -14,5 +15,13 @@ class ProductController extends Controller
 
         // 変数$productsをproducts/index.blade.phpファイルに渡す
         return view('products.index', compact('products'));
+    }
+
+    public function show($id) {
+        // URL'/products/{id}'の'{id}'部分と主キー（idカラム）の値が一致するデータをproductsテーブルから取得し、変数$productに代入する
+        $product = Product::find($id);
+
+        // 変数$productをproducts/show.blade.phpファイルに渡す
+        return view('products.show', compact('product'));
     }
 }
